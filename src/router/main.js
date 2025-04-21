f133190d726605ea02a44f2e6145b734e6d78412
julianneestras <julianneestras@gmail.com>
2025-04-21 14:24:10 +0800

    🎉 init: Project initialization

---
+++ src/router/main.js
@@ -0,0 +1,22 @@
+import { createWebHistory, createRouter } from "vue-router";
+
+import Home from "../views/Home.vue";
+
+const routes = [
+  {
+    path: "/",
+    redirect: "/home",
+  },
+  {
+    path: "/home",
+    name: "Home",
+    component: Home,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
---
+++ src/views/Home.vue
@@ -0,0 +1,7 @@
+<template>
+    <DarkMode msg="Vite + Vue" />
+</template>
+
+<script setup>
+import DarkMode from '../components/DarkMode.vue'
+</script>
