c42064427456b8206d6cefa5f6f363b28e74a10b
julianneestras <julianneestras@gmail.com>
2025-05-19 18:01:49 +0800

    ✨ feat: Add useTheme composable for managing light and dark mode

---
+++ src/composables/useTheme.js
@@ -0,0 +1,29 @@
+import { ref } from "vue";
+
+const isDark = ref(false);
+
+const setTheme = (theme) => {
+  if (theme === "dracula") {
+    document.documentElement.setAttribute("data-theme", "dracula");
+    localStorage.setItem("color-theme", "dracula");
+    isDark.value = true;
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+    localStorage.setItem("color-theme", "light");
+    isDark.value = false;
+  }
+};
+
+const initTheme = () => {
+  const stored = localStorage.getItem("color-theme");
+
+  if (stored === "dracula" || !stored === null) {
+    setTheme("dracula");
+  } else {
+    setTheme("light");
+  }
+};
+
+export function useTheme() {
+  return { isDark, setTheme, initTheme };
+}
